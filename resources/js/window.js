4313311378d35a9f3cb4a7e6f6e03c018cb6a488
unknown <24364012+EanKeen@users.noreply.github.com>
2018-05-15 20:14:31 -0700

    Grid size increased, element overview updates The main grid system increased. There are twice as many groups and periods as previously; each element now takes up four grids (two rows and two columns). Also, the div that shows overview of element being hovered over now changes (partially). The element overvier panel is not formatted either.

--- resources/js/window.js
+++ resources/js/window.js
@@ -1,7 +1,7 @@
 function redrawHeight()
 {
   var viewportHeight = document.documentElement.clientHeight;
-  // TODO: Change the height of each element in table container 
+  // TODO: Change the height of each element in table container
 
 }
 
