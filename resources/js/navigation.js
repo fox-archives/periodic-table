6e59fa031ae5c6c2c613965da6410c5b7a187a9c
unknown <24364012+EanKeen@users.noreply.github.com>
2018-05-29 10:48:46 -0700

    Added navbar constraints when swiping navbar

--- resources/js/navigation.js
+++ resources/js/navigation.js
@@ -8,22 +8,37 @@ mc.get("pan").set({ direction: Hammer.DIRECTION_ALL });
 mc.on("panleft panright", function(ev) {
   // Get nav content bar DOM element
   var navigationDOM = document.getElementById("nav-content");
-  //console.log(navigationDOM);
+
   // Get current margin-left of navigation bar (as a fllat)
-  var navMargin = parseFloat(window.getComputedStyle(navigationDOM).getPropertyValue("margin-left"));
+  var currentNavMargin = parseFloat(window.getComputedStyle(navigationDOM).getPropertyValue("margin-left"));
 
   var velocityMultiplier = 25;
   var windowWidth = document.documentElement.clientWidth;
-  var navBarWidth = navigationDOM.clientWidth;
-  console.log("window " + windowWidth);
-  console.log("nav bar " + navBarWidth);
+
+  // Alternate way of measuring width of nav bar
+  //var navBarWidth = navigationDOM.clientWidth;
+  var navBarWidth = navigationDOM.getBoundingClientRect().width;
 
   // Only allow movement of nav if nav bar length is greater than window width
   if(navBarWidth > windowWidth) {
-    var newNavMargin = navMargin + (velocityMultiplier * ev.velocityX);
+    var newNavLeft = currentNavMargin + (velocityMultiplier * ev.velocityX);
+    var newNavRight = currentNavMargin + navBarWidth + (velocityMultiplier * ev.velocityX);
 
+    // Only change margin of narbar if the new nav margin is calculated to be less than zero
+    if(newNavLeft <= 0 && newNavRight >= windowWidth) {
+      document.getElementById("nav-content").style.marginLeft = newNavLeft + "px";
+    }
+    else if (newNavLeft > 0) {
+      newNavLeft = 0;
+      document.getElementById("nav-content").style.marginLeft = newNavLeft + "px";
+    }
+    else if (newNavRight < windowWidth) {
+      // The margin of the element is equal to the length of the div element minus window length
+      newNavLeft = -(navBarWidth - windowWidth);
+      document.getElementById("nav-content").style.marginLeft = newNavLeft + "px";
+      //document.getElementById("nav-content").style.marginLeft = 0 + "px";
+    }
 
-        document.getElementById("nav-content").style.marginLeft = newNavMargin + "px";
   }
   else {
     // Window width is greatr than nav bar, which means we don't need to move nav bar
