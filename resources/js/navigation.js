f2e66c779ef34e3fd8c635b9ce7166134cca0ec0
unknown <24364012+EanKeen@users.noreply.github.com>
2018-05-29 00:36:43 -0700

    Slide navbar iff navbar has less width than browser The navigation bar can only be moved if and only if the navigation bar has a smaller width than the width of the browser window.

--- resources/js/navigation.js
+++ resources/js/navigation.js
@@ -6,13 +6,26 @@ var mc = new Hammer(navElement);
 mc.get("pan").set({ direction: Hammer.DIRECTION_ALL });
 
 mc.on("panleft panright", function(ev) {
-  //console.log(ev);
+  // Get nav content bar DOM element
   var navigationDOM = document.getElementById("nav-content");
-  var navMargin = window.getComputedStyle(navigationDOM).getPropertyValue("margin-left");
-  //if(parseFloat(navMargin) >= 0) {
-    console.log(navMargin);
-    var newNavMargin = parseFloat(navMargin) + (10 * ev.velocityX) + "px";
-    document.getElementById("nav-content").style.marginLeft = newNavMargin;
-    //navElement.textContent = ev.type + " gesture detected.";
-  //}
+  //console.log(navigationDOM);
+  // Get current margin-left of navigation bar (as a fllat)
+  var navMargin = parseFloat(window.getComputedStyle(navigationDOM).getPropertyValue("margin-left"));
+
+  var velocityMultiplier = 25;
+  var windowWidth = document.documentElement.clientWidth;
+  var navBarWidth = navigationDOM.clientWidth;
+  console.log("window " + windowWidth);
+  console.log("nav bar " + navBarWidth);
+
+  // Only allow movement of nav if nav bar length is greater than window width
+  if(navBarWidth > windowWidth) {
+    var newNavMargin = navMargin + (velocityMultiplier * ev.velocityX);
+
+
+        document.getElementById("nav-content").style.marginLeft = newNavMargin + "px";
+  }
+  else {
+    // Window width is greatr than nav bar, which means we don't need to move nav bar
+  }
 })
