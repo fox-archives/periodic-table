34f3144d0a8ceb1a1491e204585adfec12175c0f
unknown <24364012+EanKeen@users.noreply.github.com>
2018-06-18 21:30:30 -0700

    Update dist

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,11 @@ module.exports = {
        },
        {
           test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "url-loader?limit=10000&mimetype=application/font-woff"
+          loader: "url-loader?limit=10000&mimetype=application/font-woff",
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets'
+          }
        },
        {
          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
