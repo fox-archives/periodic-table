e5f501d00dda03c93e09d4cd63f5e87e53d8e61c
unknown <24364012+EanKeen@users.noreply.github.com>
2018-06-27 19:30:57 -0700

    Build structure of /dist and file paths fixed (2) Second try. For some reason, these work on the local index.html file, but not the GitHub pages website.

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
-      publicPath: '/dist'
+      /*publicPath: '/dist'*/
   },
    module: {
       rules: [
