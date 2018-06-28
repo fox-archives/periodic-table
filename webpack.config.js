df3bed0575cfd22bd4d6ca93e0ebf81d129f5cfa
unknown <24364012+EanKeen@users.noreply.github.com>
2018-06-27 21:50:54 -0700

    Build structure of /dist and file paths fixed (3)

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
-      /*publicPath: '/dist'*/
+      publicPath: ''
   },
    module: {
       rules: [
@@ -58,7 +58,7 @@ module.exports = {
            loader: 'file-loader',
            options: {
              name: '[name].[ext]',
-             outputPath: '/assets'
+             outputPath: 'assets'
            }
         },
         {
