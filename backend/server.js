8cafe407b06d663f13e338c72bd3047839047305
EanKeen <24364012+EanKeen@users.noreply.github.com>
2018-07-24 23:58:50 -0700

    Fix typo 'rs' typed rather than 'res'

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express()
 
 // Respond with "Hello World" on get request to the home page
 app.get('/', function(req, res) {
-  rs.send('Hello World')
+  res.send('Hello World')
 })
 
 app.listen(3000, () => console.log('Example App'))
