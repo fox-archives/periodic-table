d6a2b01ebbbf3a26a921c7986a240b2e2b1a4760
EanKeen <24364012+EanKeen@users.noreply.github.com>
2018-09-26 23:37:47 -0700

    Update gulp file, gzips and brotlizes assets Extracts the compressed, gzipped and brotilized assets to proper folders

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,11 +1,27 @@
 var gulp = require('gulp');
+var jsonminify = require('gulp-jsonminify');
+var gzip = require('gulp-gzip');
+var brotli = require('gulp-brotli');
 
-gulp.task('deploy', function() {
+gulp.task('deploy-html', function() {
   gulp.src('./dist/index.html')
-  .pipe(gulp.dest('../backend'))
+  .pipe(gulp.dest('../backend'));
 });
 
-gulp.task('copy-json', function() {
-  gulp.src('../computed/simple.json')
-  .pipe(gulp.dest('../backend'))
+gulp.task('deploy-json', function() {
+  // Pipe uncompressed and compressed assets
+  gulp.src('../computed/*.json')
+  .pipe(gulp.dest('../backend/uncompressed-assets/data'))
+  .pipe(jsonminify())
+  .pipe(gulp.dest('../backend/assets/data'));
+
+  // Pipe compressed, then gziped assets
+  gulp.src('../computed/*.json')
+  .pipe(gzip())
+  .pipe(gulp.dest('../backend/assets/data'));
+
+  // Pipe compressed, then brotlized assets
+  gulp.src('../computed/*.json')
+  .pipe(brotli.compress())
+  .pipe(gulp.dest('../backend/assets/data'));
 });
