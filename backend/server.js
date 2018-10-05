439302bba1e6a54c9b235e4408c5aa9731137e48
EanKeen <24364012+EanKeen@users.noreply.github.com>
2018-10-04 20:45:54 -0700

    Remove compression from backend

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,17 @@
 const express = require('express');
-const compression = require('compression');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
-const cors = require('cors');
+// const cors = require('cors');
+// app.use(cors());
 const path = require('path');
 
 const app = express();
-app.use(compression()); // Allow for Gzip compression
 app.use(bodyParser.json()); // Allow express to parse .json requests sent in
 app.use(morgan('combined')); // Use Morgan log generator
 
 // app.use(express.static('public')); // This replaces the bottom two lines
 app.use('/old', express.static(path.join(__dirname, 'public/old')));
 app.use('/element-data', express.static(path.join(__dirname, 'public/element-data')));
-
-app.get('*.json', function(req, res, next) {
-  req.url = req.url + '.gz';
-  res.set('Content-Encoding', 'gzip');
-});
-
-// app.use(cors());
 
 // Respond with 'Working' on get request to the home page
 app.get('/', function(req, res) {
