b99c53140aac28509c2aeffee4617a25c39b8bda
EanKeen <24364012+EanKeen@users.noreply.github.com>
2018-10-06 17:29:03 -0700

    Modify build process and deploy Modify gulp file that channels created .json files to backend

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,6 +3,9 @@ var jsonminify = require('gulp-jsonminify');
 var gzip = require('gulp-gzip');
 var brotli = require('gulp-brotli');
 
+const inDir = '../computed/json/*.json';
+const outDir = '../backend/public/element-data';
+
 gulp.task('deploy-html', function() {
   gulp.src('./dist/index.html')
   .pipe(gulp.dest('../backend'));
@@ -11,19 +14,19 @@ gulp.task('deploy-html', function() {
 // TODO: Clear out files before copying new ones over
 gulp.task('deploy-json', function() {
   // Pipe uncompressed and compressed assets
-  gulp.src('../computed/json/*.json')
+  gulp.src(inDir)
   .pipe(jsonminify())
-  .pipe(gulp.dest('../backend/assets/data'));
+  .pipe(gulp.dest(outDir));
 
   // Pipe compressed, then gziped assets
-  gulp.src('../computed/json/*.json')
+  gulp.src(inDir)
   .pipe(jsonminify())
   .pipe(gzip())
-  .pipe(gulp.dest('../backend/assets/data'));
+  .pipe(gulp.dest(outDir));
 
   // Pipe compressed, then brotlized assets
-  gulp.src('../computed/json/*.json')
+  gulp.src(inDir)
   .pipe(jsonminify())
   .pipe(brotli.compress())
-  .pipe(gulp.dest('../backend/assets/data'));
+  .pipe(gulp.dest(outDir));
 });
