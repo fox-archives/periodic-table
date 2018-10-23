4750b53f242cd21fda040483941249f8842447b7
EanKeen <24364012+EanKeen@users.noreply.github.com>
2018-10-23 01:06:36 -0700

    :rocket: Actually deploy js and css Change reflected in gulp deploy script (renamed deploy-html to deploy-dist)

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -6,9 +6,15 @@ var brotli = require('gulp-brotli');
 const inDir = '../computed/json/*.json';
 const outDir = '../backend/public/element-data';
 
-gulp.task('deploy-html', function() {
+gulp.task('deploy-dist', function() {
   gulp.src('./dist/index.html')
   .pipe(gulp.dest('../backend'));
+
+  gulp.src('./dist/bundle.js')
+    .pipe(gulp.dest('../backend'));
+
+  gulp.src('./dist/bundle.css')
+    .pipe(gulp.dest('../backend'));
 });
 
 // TODO: Clear out files before copying new ones over
