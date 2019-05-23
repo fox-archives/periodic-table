e68812a67e43035743f72ffda6b2fbcfaf69d972
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-05-22 17:21:39 -0700

    fix: lint

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 // const cors = require('cors');
 // app.use(cors());
-const path = require('path');
+const path = require("path");
 
 const app = express();
 app.use(bodyParser.json()); // Allow express to parse .json requests sent in
-app.use(morgan('combined')); // Use Morgan log generator
+app.use(morgan("combined")); // Use Morgan log generator
 
 // app.use(express.static('public')); // This replaces the bottom two lines
-app.use('/old', express.static(path.join(__dirname, 'public/old')));
-app.use('/element-data', express.static(path.join(__dirname, 'public/element-data')));
-app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+app.use("/old", express.static(path.join(__dirname, "public/old")));
+app.use(
+  "/element-data",
+  express.static(path.join(__dirname, "public/element-data"))
+);
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // Respond with 'Working' on get request to the home page
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'), function(err) {
-
-  });
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"), function(err) {});
 });
 
-app.get('/bundle.js', function(req, res) {
-  res.sendFile(path.join(__dirname, '/bundle.js'), function(err) {
-
-  });
+app.get("/bundle.js", function(req, res) {
+  res.sendFile(path.join(__dirname, "/bundle.js"), function(err) {});
 });
 
-app.get('/bundle.css', function(req, res) {
-  res.sendFile(path.join(__dirname, '/bundle.css'), function(err) {
-
-  });
+app.get("/bundle.css", function(req, res) {
+  res.sendFile(path.join(__dirname, "/bundle.css"), function(err) {});
 });
 
 // Catch-all request (if user goes to a URL that only the client can resolve (AKA vue-router),
 // send the user the .html file, rather than trying to resolve the URL server-side
 // If this is disabled, would get something like 'Cannot GET /orbitals, if client navigates to /orbitals tab'
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'), function(err) {
-
-  });
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"), function(err) {});
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Working'));
+app.listen(port, () => console.log("Working"));
