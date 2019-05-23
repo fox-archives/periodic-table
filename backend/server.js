f0177f679327fee8419ef452d95f76eabe7b323a
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-05-22 17:30:44 -0700

    polish: es6ify

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,14 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const fs = require("fs");
-// const cors = require('cors');
-// app.use(cors());
-const path = require("path");
+let express = require("express");
+let bodyParser = require("body-parser");
+let logger = require("morgan");
+let fs = require("fs");
+let path = require("path");
 
-const app = express();
+let app = express();
 app.use(bodyParser.json()); // Allow express to parse .json requests sent in
-app.use(morgan("common")); // Use Morgan log generator
+app.use(logger("common")); // Use Morgan log generator
 app.use(
-  morgan("common", {
+  logger("common", {
     stream: fs.createWriteStream("./morgan.log", {
       flags: "a"
     })
@@ -26,23 +24,23 @@ app.use(
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // Respond with 'Working' on get request to the home page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"), function(err) {});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"), err => {});
 });
 
-app.get("/bundle.js", function(req, res) {
-  res.sendFile(path.join(__dirname, "/bundle.js"), function(err) {});
+app.get("/bundle.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "/bundle.js"), err => {});
 });
 
-app.get("/bundle.css", function(req, res) {
-  res.sendFile(path.join(__dirname, "/bundle.css"), function(err) {});
+app.get("/bundle.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "/bundle.css"), err => {});
 });
 
 // Catch-all request (if user goes to a URL that only the client can resolve (AKA vue-router),
 // send the user the .html file, rather than trying to resolve the URL server-side
 // If this is disabled, would get something like 'Cannot GET /orbitals, if client navigates to /orbitals tab'
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"), function(err) {});
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"), err => {});
 });
 
 const port = process.env.PORT || 3000;
