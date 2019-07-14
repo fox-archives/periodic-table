dd57ae316cd13ff6a9d593ff40f20d1053e87ee2
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-07-14 00:07:03 -0700

    refactor: improve wolf lang src fles and gen. json

---
+++ computed/generateAtomData.sh
@@ -0,0 +1,16 @@
+cd atom-colors
+wolframscript -script atomColorsCategory.wls
+wolframscript -script atomColorsOrbitalBlock.wls
+cd ..
+
+cd atom-placement
+wolframscript -script atomPlacement.wls
+cd ..
+
+cd atom-tab-isotopes
+wolframscript -script atomTabIsotopes.wls
+cd ..
+
+cd atom-tab-properties
+wolframscript -script atomTabProperties.wls
+cd ..
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+let path = require("path");
+
+let { dest, src } = require("gulp");
+let flatten = require("gulp-flatten");
+let jsonminify = require("gulp-jsonminify");
+
+let del = require("del");
+
+async function transferComputed() {
+  let from = "computed/**/*.json";
+  let to = "backend/public/data";
+  
+  del([to + "/**", "!" + to])
+    .then(() => {
+      src(from)
+        .pipe(flatten())
+        .pipe(jsonminify())
+        .pipe(dest(to))
+    })
+}
+
+exports.transferComputed = transferComputed;
