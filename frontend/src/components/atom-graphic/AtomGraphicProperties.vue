731c2080671049dea2b925a215e0866d04968e1c
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-07-16 19:47:30 -0700

    fix: atom graphic text updates slowly not the atom grahpic text is set through css only, as a fraction of the viewport

--- frontend/src/components/atom-graphic/AtomGraphicProperties.vue
+++ frontend/src/components/atom-graphic/AtomGraphicProperties.vue
@@ -15,37 +15,11 @@
 
 <script>
 import { mapState } from 'vuex';
-import { EventBus } from '@/eventBus';
-import throttle from 'lodash/debounce';
 
 export default {
   name: 'AtomGraphicProperties',
-  watch: {
-    $route() {
-      this.newSizeInfoVisualText();
-    }
-  },
-  mounted() {
-    //TODO: Fix this bug with adding delay?
-    setTimeout(() => this.newSizeInfoVisualText(), 20);
-    // this.newSizeInfoVisualText();
-
-    window.addEventListener('resize', throttle(this.newSizeInfoVisualText, 50));
-
-    EventBus.$on('set-info-location', payload => {
-      this.newSizeInfoVisualText();
-    });
-  },
   computed: {
-    ...mapState(['atomActiveSimpleData', 'options'])
-  },
-  methods: {
-    newSizeInfoVisualText: function() {
-      // container is the large color box with large abbreviation, and the area around that (the square)
-      let container = document.getElementById('visual-inner');
-      let fontSize = container.clientWidth * 0.12 + 'px';
-      container.style.setProperty('--elementNameTextSize', fontSize);
-    }
+    ...mapState(['atomActiveSimpleData'])
   }
 };
 </script>
