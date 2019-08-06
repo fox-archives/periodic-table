077e6c7ea8591249a352284f5ddeb654db73c192
eankeen <24364012+eankeen@users.noreply.github.com>
2019-08-05 17:01:53 -0700

    feat: create working eslint config works with babel, prettier, and vue combo

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,23 +1,10 @@
-// module.exports = {
-//   env: {
-//     node: true,
-//     browser: true,
-//     commonjs: true,
-//     es6: true
-//   },
-//   extends: [
-//     'eslint:recommended',
-//     'plugin:vue/recommended',
-//     'plugin:prettier/recommended',
-//     'prettier'
-//   ],
-//   parserOptions: {
-//     parser: 'babel-eslint',
-//     ecmaVersion: 2017
-//   },
-//   plugins: ['prettier', 'babel'],
-//   rules: {
-//     'prettier/prettier': 'error',
-//     'vue/max-attributes-per-line': 'off'
-//   }
-// };
+module.exports = {
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: ['plugin:vue/recommended', 'plugin:prettier/recommended'],
+  plugins: [
+    'babel',
+    'vue'
+  ]
+};
