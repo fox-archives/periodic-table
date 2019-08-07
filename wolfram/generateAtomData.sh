7dcd4b0117f378d4488d8a0bce1e3aae87a2b504
eankeen <24364012+eankeen@users.noreply.github.com>
2019-08-06 22:04:56 -0700

    docs: improve documentation of wolfram folder

--- wolfram/generateAtomData.sh
+++ wolfram/generateAtomData.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-cd atom-colors
+cd atom-colors || exit
 wolframscript -script atomColorsCategory.wls
 wolframscript -script atomColorsOrbitalBlock.wls
 cd ..
 
-cd atom-placement
+cd atom-placement || exit
 wolframscript -script atomPlacement.wls
 cd ..
 
-cd atom-tab-isotopes
+cd atom-tab-isotopes || exit
 wolframscript -script atomTabIsotopes.wls
 cd ..
 
-cd atom-tab-properties
+cd atom-tab-properties || exit
 wolframscript -script atomTabProperties.wls
 cd ..
