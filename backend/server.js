bb3a6e12db40226ebc9ee5c72d043f23811814a2
eankeen <24364012+eankeen@users.noreply.github.com>
2019-12-20 16:47:38 -0800

    polish: remove unused body parser in backend only used for converting to json of incoming post requests

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
 import path from "path";
 import express from "express";
-import bodyParser from "body-parser";
 import morgan from "morgan";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "../" });
 
 const app = express();
+app.set('json spaces', 2)
+app.disable("strict routing")
+app.disable("'x-powered-by");
 
 app.use(morgan('tiny'));
-app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"), e => console.log(e));
+  res.sendFile(path.join(__dirname, "public", "index.html"), err => console.log(err));
 });
 
 const port = process.env.PORT || 3000;
